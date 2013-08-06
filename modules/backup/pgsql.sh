d9d477144c7b2ce58ff557a059f96e6dde00204b
Dan Fruehauf <malkodan@gmail.com>
2013-08-07 00:56:59 +1000

    some better documentation for pgsql module

--- modules/backup/pgsql.sh
+++ modules/backup/pgsql.sh
@@ -3,15 +3,17 @@
 # Written by Dan Fruehauf <malkodan@gmail.com>
 
 # backup for a pgsql database
-# $1 - credentials and settings for pg_dump
+# $1 - credentials and settings for pg_dump, a colon separated tuple
 # "$@" - parameters for pg_dump
 execute() {
+	# credentials will be in the form of:
+	# HOSTNAME:PORT:DB:USERNAME:PASSWORD
 	local credentials=$1; shift
 	local host=`echo $credentials | cut -d: -f1`
 	local port=`echo $credentials | cut -d: -f2`
 	local name=`echo $credentials | cut -d: -f3`
 	local user=`echo $credentials | cut -d: -f4`
-	local pass=`echo $credentials | cut -d: -f5`
+	local pass=`echo $credentials | cut -d: -f5-`
 	logger_info "PgSQL backup initiated with credentials: '$credentials', extra parameters: '$@'"
 
 	local -i retval
