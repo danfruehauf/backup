a2262509644a3b68df6b6145133be4ebe3f9bae0
Dan Fruehauf <malkodan@gmail.com>
2013-08-07 23:19:32 +1000

    added scp store module

---
+++ modules/store/scp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Written by Dan Fruehauf <malkodan@gmail.com>
+
+# copies backup with rsync
+# $1 - backup destination
+# "$@" - rsync parameters
+execute() {
+	local backup_name=$1; shift
+	local scp_destination=$1; shift
+	local backup_destination="$scp_destination/$backup_name-"`date '+%Y.%m.%d.%H.%m.%S'`
+	logger_info "Using scp to copy backup to '$backup_destination'"
+	scp -r "$@" $_BACKUP_DEST "$backup_destination"
+}
+
