117d20b6e40413860f5242f813aa8c4aa9642cf5
Dan Fruehauf <malkodan@gmail.com>
2013-08-08 01:22:24 +1000

    added gpg encryption

---
+++ modules/process/gpg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# suffix to add to files encrypted with gpg
+declare -r GPG_SUFFIX=gpg
+
+# encrypts files with gpg
+# $1 - regexp for files to encrypt with gpg
+# $2 - key file to encrypt with
+execute() {
+	local regexp="$1"; shift
+	local key_file=$1; shift
+
+	# import key
+	local tmp_gpg_homedir=`mktemp -d`
+	if ! _import_gpg_key $tmp_gpg_homedir $key_file; then
+		logger_fatal "Failed to import gpg key '$tmp_keyring'"
+	fi
+
+	# get the key id, this is the recipient field when encrypting
+	local key_id=`gpg --homedir $tmp_gpg_homedir --list-keys | \
+		grep '^pub\b' | tr -s " " | cut -d' ' -f2 | cut -d/ -f2`
+
+	# bail on any error here...
+	set -e
+
+	# operate on all files in backup directory that match the given regexp
+	local file
+	for file in `find $_BACKUP_DEST -type f -regex "$regexp"`; do
+		# encrypt with gpg
+		gpg --homedir $tmp_gpg_homedir \
+			--batch -q --yes \
+			--trust-model always --encrypt \
+			--recipient $key_id \
+			--output $file.$GPG_SUFFIX $file
+
+		# preserve permissions of file after encryption
+		chown --reference=$file $file.$GPG_SUFFIX
+		chmod --reference=$file $file.$GPG_SUFFIX
+
+		# remove the old file
+		rm -f $file
+	done
+	set +e
+
+	rm -rf --preserve-root $tmp_gpg_homedir
+}
+
+# imports a gpg key
+# $1 - gpg temporary homedir
+_import_gpg_key() {
+	local gpg_homedir=$1; shift
+	local key_file=$1; shift
+	gpg --homedir $gpg_homedir --import $key_file
+}
--- modules/process/gzip.sh
+++ modules/process/gzip.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # gzips files in directory
+# $1 - regexp for files to gzip
 # "$@" - arguments for gzip
 execute() {
 	local regexp="$1"; shift
-	# operate on all files in backup directory that match the give regexp
+	# operate on all files in backup directory that match the given regexp
 	find $_BACKUP_DEST -type f -regex "$regexp" -exec gzip {} \;
 }
